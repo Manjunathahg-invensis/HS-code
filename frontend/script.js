dd08910c7ddd4b5fe91652d0f73c9bc3fbf002c6
Manjunathahg-invensis <manjunatha.hg@phygital-insights.com>
2024-09-11 09:49:20 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -6,7 +6,7 @@ async function displayOutput() {
 
     // Send POST request to the FastAPI endpoint
     try {
-        const response = await fetch('http://127.0.0.1:8000/query/', {
+        const response = await fetch('https://127.0.0.1:8000/query/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
